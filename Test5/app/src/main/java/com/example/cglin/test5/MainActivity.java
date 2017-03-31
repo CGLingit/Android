6d626f6ff57ad75027f030717fc8bda182c64060
LCG_GIT <1542102449@qq.com>
2017-03-31 17:25:04 +0800

    控件    RadioGroup    CheckBox
     提示信息
       Toast

---
+++ Test5/app/src/main/java/com/example/cglin/test5/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.cglin.test5;
+
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    //对控件对象进行声明
+
+    private RadioGroup genderGroup=null;
+    private RadioButton femaleButton=null;
+    private RadioButton maleButton=null;
+    private CheckBox swimBox=null;
+    private CheckBox runBox=null;
+    private CheckBox readBox=null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //通过控件的ID得到代表控件的对象
+        genderGroup= (RadioGroup) findViewById(R.id.genderGroup);
+        femaleButton=(RadioButton) findViewById(R.id.femaleButton);
+        maleButton=(RadioButton) findViewById(R.id.maleButton);
+        swimBox=(CheckBox) findViewById(R.id.swimbox);
+        runBox=(CheckBox)findViewById(R.id.runbox);
+        readBox=(CheckBox)findViewById(R.id.readbox);
+
+        genderGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if(femaleButton.getId()==checkedId){
+                    System.out.println("famale");
+                    //提示信息
+                    Toast.makeText(MainActivity.this,"female",Toast.LENGTH_SHORT).show();
+                }else if(maleButton.getId()==checkedId){
+                    System.out.println("male");
+                }
+            }
+        });
+
+        swimBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    System.out.println("swim is checked");
+                }
+                else
+                {
+                    System.out.println("unchecked");
+                }
+            }
+        });
+        runBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    System.out.println("swim is checked");
+                }
+                else
+                {
+                    System.out.println("unchecked");
+                }
+            }
+        });
+
+        readBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked)
+                {
+                  System.out.println("swim is checked");
+             }
+            else
+            {
+                System.out.println("unchecked");
+            }
+          }
+        });
+
+
+
+    }
+}
