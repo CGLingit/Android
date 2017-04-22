aa285bd2cf0e0b9b44f6fa50e4da6b25eddb81e7
LCG_GIT <1542102449@qq.com>
2017-04-23 00:56:43 +0800

    实现按钮锁屏功能
    在lock的按钮上设置监听器，当点击时发出广播
    新建一个AdminManageReceiver 广播接收器
    Manifest注册
    
    （加强WindowManager类的学习）

--- XFBall/app/src/main/java/com/example/cglin/xfball/FloatWindowBigView.java
+++ XFBall/app/src/main/java/com/example/cglin/xfball/FloatWindowBigView.java
@@ -1,5 +1,8 @@
 package com.example.cglin.xfball;
 
+import android.app.admin.DevicePolicyManager;
+import android.content.ComponentName;
+import android.os.Bundle;
 import android.widget.LinearLayout;
 import android.content.Context;
 import android.content.Intent;
@@ -8,11 +11,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
+import static android.content.Context.DEVICE_POLICY_SERVICE;
+
 /**
  * Created by Administrator on 2017/4/12.
  */
 
 public class FloatWindowBigView extends LinearLayout {
+
+    ComponentName mAdminName;
+    DevicePolicyManager mDPM;
     /**
      * 记录大悬浮窗的宽度
      */
@@ -29,25 +37,27 @@ public class FloatWindowBigView extends LinearLayout {
         View view = findViewById(R.id.float_window_big);
         viewWidth = view.getLayoutParams().width;
         viewHeight = view.getLayoutParams().height;
-        Button close = (Button) findViewById(R.id.close);
         Button back = (Button) findViewById(R.id.back);
-        close.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // 点击关闭悬浮窗的时候，移除所有悬浮窗，并停止Service
-                MyWindowManager.removeBigWindow(context);
-                MyWindowManager.removeSmallWindow(context);
-                Intent intent = new Intent(getContext(), FloatWindowService.class);
-                context.stopService(intent);
-            }
-        });
+        Button lock=(Button)findViewById(R.id.lock);
         back.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
                 // 点击返回的时候，移除大悬浮窗，创建小悬浮窗
                 MyWindowManager.removeBigWindow(context);
                 MyWindowManager.createSmallWindow(context);
+
+
+            }
+        });
+        lock.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DevicePolicyManager devicePolicyManager = (DevicePolicyManager) context.getSystemService(DEVICE_POLICY_SERVICE);
+                MyWindowManager.removeBigWindow(context);
+                MyWindowManager.createSmallWindow(context);
+                devicePolicyManager.lockNow();
             }
         });
     }
+
 }
